19eb840f465ed6e042af105831477fa347b122f1
Helen He <hetian@bc.edu>
2026-02-15 15:29:51 -0500

    Initial SQL structure

--- sql-revenue-analysis/sql/01_create_tables.sql
+++ sql-revenue-analysis/sql/01_create_tables.sql
@@ -1,1 +1,10 @@
+-- Revenue KPI SQL Project
+-- Author: Tianxiao He
+-- Date: 2026-02-15
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    signup_date DATE,
+    channel VARCHAR(50)
+);
 
